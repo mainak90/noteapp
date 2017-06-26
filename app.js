a74fdf8fe23e8642763aaffb4ce4b3f84b5b19ee
Mainak Dhar <mainak90@gmail.com>
2017-06-27 03:52:27 +0530

    1st commit

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log('Starting app.js');
+
+const fs = require('fs');
+const _ = require('lodash');
+const yargs = require('yargs');
+const notes = require('./notes.js');
+
+const argv = yargs.argv;
+
+var command = process.argv[2];
+console.log('command: ', command);
+console.log('Yargs: ', argv);
+
+if (command === 'add') {
+    notes.addNote(argv.title, argv.body);
+} else if (command === 'list') {
+    notes.getAll();
+} else if (command === 'read') {
+    notes.getNote(argv.title);
+} else if (command === 'remove') {
+    notes.removeNote(argv.title);
+} else {
+    console.log('Input undefined');
+}
---
+++ playground/json.js
@@ -0,0 +1,28 @@
+// var obj = {
+//     name : 'Mainak'
+// }
+
+// var stringObj = JSON.stringify(obj);
+
+// console.log(typeof stringObj);
+// console.log(stringObj);
+
+// var personString = '{"name" : "Mainak", "age" : 27}';
+// var person = JSON.parse(personString);
+// console.log(typeof person);
+// console.log(person);
+
+const fs = require('fs');
+
+var originalNote = {
+    title : 'SomeTitle',
+    body : 'Some body'
+};
+var originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json', originalNoteString);
+
+var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(noteString);
+console.log(typeof note);
+console.log(note);
+
